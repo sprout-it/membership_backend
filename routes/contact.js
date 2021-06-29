8b44337c7e3d96aee512a0c95d3ffc0bad7b93f6
GuyAK10 <akkaraphol.khunpet@gmail.com>
2021-06-30 00:02:14 +0700

    fix get contract

--- routes/contact.js
+++ routes/contact.js
@@ -1,28 +1,28 @@
 const express = require("express");
 const { firestore } = require("../configs/firebase");
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const fs = require('fs')
-const privateKey = fs.readFileSync('./configs/private.pem', 'utf8');
 
 router.get("/contract", async (req, res) => {
-    try {
-      res.status(200).json("Hello World")
-    } catch (error) {
-      console.log(error);
-    }
-  });
+  try {
+    let contract = []
+    const contractRef = await firestore.collection("order").get()
+    await Promise.all(contractRef.docs.map((data)=>{
+      let obj = {
+        docId:data.id,
+        contractData:data.data()
+      }
+      contract.push(obj)
+    }))
+    res.status(200).send(contract);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 router.post("/contract", async (req, res) => {
   try {
-    // await firestore.collection("order").add(req.body);
-    const payload = {
-        name:"กาย" ,
-        price: 3000,
-        exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24)
-  }
-  let encoded = jwt.sign(payload, privateKey, { algorithm: 'HS256' });
-    res.status(200).send(encoded)
+    await firestore.collection("order").add(req.body);
+    res.status(200).send("บันทึกออเดอร์สำเร็จ");
   } catch (error) {
     console.log(error);
   }
