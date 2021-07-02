ce0c930a67f71a31e0494c7caa02179b47fbc511
GuyAK10 <akkaraphol.khunpet@gmail.com>
2021-07-02 19:09:02 +0700

    add requirement aeteam

--- routes/order.js
+++ routes/order.js
@@ -3,17 +3,29 @@ const { firestore } = require("../configs/firebase");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    try {
-      res.status(200).json("Hello World")
-    } catch (error) {
-      console.log(error);
-    }
-  });
+  try {
+    res.status(200).json("Hello World");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// router.get("/order", async (req, res) => {
+//   try {
+//     const order = await firestore.collection("order").doc("hI8ZwP0ImILlufnjbuhE").get()
+//     const a = Object.assign(order.data(),{requirement:[]})
+//     console.log(a)
+//     res.status(200).json(order.data())
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 router.post("/order", async (req, res) => {
   try {
-    await firestore.collection("order").add(req.body);
-    res.status(200).json({message:"บันทึกสำเร็จ"})
+    const order = Object.assign(req.body, { requirement: [],ae_team:[] });
+    await firestore.collection("order").add(order);
+    res.status(200).json({ message: "บันทึกสำเร็จ" });
   } catch (error) {
     console.log(error);
   }
